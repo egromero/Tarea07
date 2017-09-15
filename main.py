d3e20f08bf4ab705c03f9bab6c2069c117fb100d
Erick <egromero@uc.cl>
2017-09-15 19:52:49 -0300

    fix

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 lista_user = []
 
-tokenbot = '376888244:AAEVNjpM9r-q_6KS2lPbVxC_9EzHSGuv2rE'
-url_bot = 'https://api.telegram.org/bot' + tokenbot
+url_bot = 'https://api.telegram.org/bot376888244:AAEVNjpM9r-q_6KS2lPbVxC_9EzHSGuv2rE'
 
-@app.route('/telegram', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def telegram():
     if request.method == 'POST':
         data = request.get_json()
         process_info(data, lista_user)
-        chat_id = data['message']['chat']['id']
-        print(chat_id)
-    pass
+        print('DATAAAAA', data)
 
 
 
